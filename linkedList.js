5457b0a3fe9c469209fe79ddd501bc08b5f3a9e4
Konrad <konrad.babowicz21@gmail.com>
2024-08-20 22:51:42 +0100

    created functions to append and stringify nodes

--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,78 @@
-import Node from "./node";
+import Node from "../Linked-List/node.js";
 
 export default class LinkedList{
-
-    append(value) {
-        
+    constructor(){
+        this.headNode = null;
     }
+
+    toString(){
+        let current = this.headNode;
+        let str = "";
+
+        while(current){
+            str += current.value + "";
+            current = current.nextNode;
+        }
+        return str;
+    }
+
+//append the node to the end of the list
+    append(value) {
+        //create new node
+        let newNode = new Node(value);
+
+        //if linked list is empty, make new node as head
+        if(this.headNode === null){
+            return newNode;
+        }
+
+        //store head reference in a temp variable
+        let last = this.headNode;
+
+        //traverse until last node
+        while(last.nextNode !== null){
+            last = last.newNode;
+        }
+
+        //change the pointer of the last node to the new node
+        last.newNode = newNode;
+
+        //return the head of the list
+        return this.headNode;
+    }
+
+    prepend(value){
+        let newNode = new Node(value, this.headNode);
+        this.headNode = newNode;
+    }
+
+    size(){
+
+    }
+
+    head(){
+
+    }
+
+    tail(){
+
+    }
+
+    at(index){
+
+    }
+
+    pop(){
+
+    }
+
+    contains(value){
+
+    }
+
+    find(value){
+
+    }
+
+    
 }
