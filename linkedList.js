91ead8dcf41ab9c145fbd961235053233cd038c0
Konrad <konrad.babowicz21@gmail.com>
2024-08-21 22:48:30 +0100

    Pop and find functions resolved

--- linkedList.js
+++ linkedList.js
@@ -21,7 +21,6 @@ export default class LinkedList{
         //create new node
         let newNode = new Node(value);
 
-        
         //if linked list is empty, make new node as head
         if(this.headNode === null){
             this.headNode = newNode;
@@ -40,11 +39,13 @@ export default class LinkedList{
         last.nextNode = newNode;        
     }
 
+//adds a new node containing value to the start of the list
     prepend(value){
         let newNode = new Node(value, this.headNode);
         this.headNode = newNode;
     }
 
+//returns the total number of nodes in the list
     size(){
         let size = 0;
         let current = this.headNode;
@@ -56,6 +57,7 @@ export default class LinkedList{
         return size;
     }
 
+//returns the first node in the list
     head(){
         let current = this.headNode;
 
@@ -65,6 +67,7 @@ export default class LinkedList{
         return current.value;
     }
 
+//returns the last node in the list
     tail(){
         let current = this.headNode;
 
@@ -75,6 +78,7 @@ export default class LinkedList{
         return current.value;
     }
 
+//  returns the node at the given index
     at(index){
         let current = this.headNode;
         let count = 0;
@@ -91,10 +95,32 @@ export default class LinkedList{
 
     }
 
+//remove the last node of the list
     pop(){
+        if(this.headNode === null){
+            return null;
+        }
+    //if list has only one node, return empty list
+        if(this.headNode.nextNode === null){
+            let lastNode = this.headNode;
+            this.headNode = null;
+            return lastNode;
+        }
 
+    // Traverse to the second-to-last node
+        let current = this.headNode;
+        while(current.nextNode.nextNode !== null){
+            current = current.nextNode;
+        }
+
+    // current becomes second to last node
+        const lastNode = current.nextNode; // this is the node to be removed
+        current.nextNode = null; //remove the last node by setting the second to last node to null.
+
+        return lastNode; //return the removed node
     }
 
+    //check if the list contains the value provided
     contains(value){
         let current = this.headNode;
         if(value === current.value){
@@ -104,18 +130,30 @@ export default class LinkedList{
         }
     }
 
+//find the node value at provided index . returns the index of the node containing value, or null if not found.
     find(value){
         let current = this.headNode;
-        let count = 0;
+        let index = 0;
 
         while(current !== null){
-            if(count == value){
-                return current;
+            if(current.value == value){ //compare the nodes value with provided value
+                return index; // then return the index
             }
-            count++;
-            current = current.nextNode; 
+            index++; // increment the counter
+            current = current.nextNode;  // move onto the next
         }
+
         return null;
+    }
+
+//that inserts a new node with the provided value at the given index.
+    insertAt(value, index){
+
+    }
+
+//that removes the node at the given index.
+    removeAt(index){
+
     }
   
 }
