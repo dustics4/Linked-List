5a9b634bb6757bdb995eb9a443450500a9dfaa47
dustics4 <konrad.babowicz21@gmail.com>
2024-08-21 08:16:55 +0100

    Set headnode to be the new node when the list is empty, corrected property access

--- linkedList.js
+++ linkedList.js
@@ -6,12 +6,11 @@ export default class LinkedList{
     }
 
     toString(){
-        console.log("run!");
         let current = this.headNode;
         let str = "";
 
         while(current){
-            str += current.value + "";
+            str += `( ${current.value} ) ->  `;
             current = current.nextNode;
         }
         return str;
@@ -19,14 +18,14 @@ export default class LinkedList{
 
 //append the node to the end of the list
     append(value) {
-        console.log("run!");
-
         //create new node
         let newNode = new Node(value);
 
+        
         //if linked list is empty, make new node as head
         if(this.headNode === null){
-            return newNode;
+            this.headNode = newNode;
+            return;
         }
 
         //store head reference in a temp variable
@@ -34,14 +33,11 @@ export default class LinkedList{
 
         //traverse until last node
         while(last.nextNode !== null){
-            last = last.newNode;
+            last = last.nextNode;
         }
 
         //change the pointer of the last node to the new node
-        last.newNode = newNode;
-
-        //return the head of the list
-        return this.headNode;
+        last.nextNode = newNode;        
     }
 
     prepend(value){
